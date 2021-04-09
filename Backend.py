8860b7b24bca66d2c7d166fa7a3c8d12c2539274
Greg <schab076@d.umn.edu>
2021-04-08 19:45:16 -0500

    Inital Commit

---
+++ Backend.py
@@ -0,0 +1,154 @@
+class Encoder:
+    def __init__(self):
+        print("Encoder Created")
+        self.Output = ""
+    # Gets the input to be encoded from a specified file.
+
+    def Get_Input(self):
+        return 0
+
+    def Pad_Input(self):
+        return 0
+
+    def Ceaser_Encode(self, read_buffer, Key_Offset):
+        for char in read_buffer:
+            self.Output += chr(ord(char) + int(Key_Offset))
+
+    def Vigenere_Encode(self, read_buffer, Key_buffer):
+        count = 0
+        self.Output = ""
+        for Read_Char in read_buffer:
+            # print("Readloop: Char = " + Read_Char) #Debugging print statement
+            Key_Char = Key_buffer[count]
+            count += 1
+            # print("Key_Char = : " + Key_Char) #Debugging print statment
+            if count == len(Key_buffer):
+                count = 0
+            self.Output += chr(ord(Read_Char) + int(ord(Key_Char)))
+
+    def Physical_Shift_Encode(self,  read_buffer, shift_value):
+        self.Output = ""
+        count = 0
+        while count < int(shift_value):
+            count += 1
+
+        while count < len(read_buffer):
+            #print("Current char in read_buffer" + read_buffer[count])
+            self.Output += read_buffer[count]
+            count += 1
+        count = 0
+        while count < int(shift_value):
+            #print("Current char in read_buffer" + read_buffer[count])
+            self.Output += read_buffer[count]
+            count += 1
+
+    def Columnar_Encode(self):
+        return 0
+
+    def Block_Chain_Encode(self):
+        pass
+
+
+class Decoder:
+    def __init__(self):
+        print("Decoder Created\n")
+        self.Output = ""
+    # Writes the encoded output to a specified file.
+
+    def Write_Output(self):
+        return 0
+
+    def Unpad_Input(self):
+        padded_bytes = 0
+        return padded_bytes
+
+    def Ceaser_Decode(self, read_buffer, Key_Offset):
+        for char in read_buffer:
+            self.Output += chr(ord(char) - int(Key_Offset))
+
+    def Vigenere_Decode(self, read_buffer, Key_buffer):
+        count = 0
+        self.Output = ""
+        for Read_Char in read_buffer:
+            # print("Readloop: Char = " + Read_Char) #Debugging print statement
+            Key_Char = Key_buffer[count]
+            count += 1
+            # print("Key_Char = : " + Key_Char)#Debugging print statement
+            if count == len(Key_buffer):
+                count = 0
+            self.Output += chr(ord(Read_Char) - int(ord(Key_Char)))
+
+    def Physical_Shift_Decode(self, read_buffer, shift_value):
+        count = 0
+        self.Output = ""
+        #print(len(read_buffer) - int(shift_value))
+        while count < len(read_buffer) - int(shift_value):
+            count += 1
+        while count < len(read_buffer):
+            #print("Current char in read_buffer " + read_buffer[count])
+            self.Output += read_buffer[count]
+            count += 1
+        count = 0
+        while count < len(read_buffer) - int(shift_value):
+            #print("Current char in read_buffer " + read_buffer[count])
+            self.Output += read_buffer[count]
+            count += 1
+
+    def Columnar_Decode(self):
+        return 0
+
+    def Block_Chain_Decode(self):
+        pass
+
+
+class Product:
+
+    def __init__(self):
+        self.Product_List = []
+        self.Rounds = 1
+        self.Decode = False
+
+    def add(self, encode_name):
+        pass
+
+    def remove(self, encode_name):
+        pass
+
+    def view(self):
+        print(self.Product_List)
+
+    def clear(self):
+        self.Product_List = []
+        self.Rounds = 1
+        self.Decode = False
+
+    def set_rounds(self, New_Rounds):
+        self.Rounds = New_Rounds
+
+
+class Buffer:
+    def __init__(self):
+        buffer = []
+
+    def Convert_To_List(self, Input):
+        self.buffer = []
+        count = 0
+        for char in Input:
+            self.buffer.append(char)
+            count += 1
+        self.length = count
+        return self.buffer
+
+    def Get_Element(self, Index):
+        return self.buffer[Index]
+
+    def Print_Buffer(self):
+        print(self.buffer)
+# Transposes the input matrix and writes the transposed to output.
+
+
+def Transpose(Input, Output):
+    Output = Input
+    return Output
+
+# Getter for rounds
---
+++ Frontend.py
@@ -0,0 +1,113 @@
+from Backend import Encoder, Decoder, Buffer, Product
+
+
+def Welcome_Screen():
+    print("Hello, and thank you for using this encoding script!\n")
+    Options()
+    Options_Handling()
+
+
+def Options():
+    print("\n1: info")
+    print("2: Ceaser")
+    print("3: Vigenere")
+    print("4: Physical shift")
+    print("q: Quit\n")
+
+
+def Options_Handling():
+    check = True
+    while check == True:
+        feedback = input(
+            "Please enter one of the commands from above to perform the given task.\n")
+        if feedback == '1':
+            Info_Screen()
+        elif feedback == '2':
+            Ceaser()
+            Options()
+            Options_Handling()
+        elif feedback == '3':
+            Vigenere()
+            Options()
+            Options_Handling()
+        elif feedback == '4':
+            Physical_Shift()
+            Options()
+            Options_Handling()
+        elif feedback.upper() == 'Q':
+            exit()
+
+
+def Info_Screen():
+    print("info stuff stuff")
+    Options()
+    Options_Handling()
+
+
+def Ceaser():
+    print("The Ceaser Cipher is a very old encryption algorithm that relies upon shifting each characted by a given value.")
+    Input = input("Please Provide a string that you would like to encrypt\n")
+    Input_List = Read_buffer.Convert_To_List(Input)
+    Shift = input(
+        "Please provide an integer that you would like to shift by\n")
+    Encoder.Ceaser_Encode(Input_List, Shift)
+    print("Ceaser encoded string: " + Encoder.Output)
+
+    feedback = input(
+        "\nWould you like to decrypt the encoded string to show that it worked properly?\n")
+    if feedback.upper() == 'Y':
+        Decoder.Ceaser_Decode(Encoder.Output, Shift)
+        print("Decoded Ceaser encoded string: " + Decoder.Output)
+
+    input("press any key to continue... \n")
+
+
+def Vigenere():
+    print("The Vigenere Cipher is an extension of the Ceaser cipher. The difference is that\n"
+          "we are given a string key and then we take the ascii value of the key char and add\n"
+          "it onto the ascii value of the input char resulting in a new char which is our encoded\n"
+          "text.\n")
+
+    Input = input("Please Provide a string that you would like to encrypt\n")
+    Input_List = Read_buffer.Convert_To_List(Input)
+    Key = input("Please enter a key that you would like to use. Ex.'purple'\n")
+    Key_list = Key_buffer.Convert_To_List(Key)
+    Encoder.Vigenere_Encode(Input_List, Key_list)
+    print("The Vigenere encoded text is: " + Encoder.Output)
+    feedback = input(
+        "Would you like to decrypt the encoded string to show that it worked properly?\n")
+    if feedback.upper() == 'Y':
+        Decoder.Vigenere_Decode(Encoder.Output, Key_list)
+        print("Decoded Ceaser encoded string: " + Decoder.Output)
+    input("press any key to continue... \n")
+
+
+def Physical_Shift():
+    print("The Physical Shift cipher is quite a basic one as it does exactly what the name\n"
+          "suggests. We take an input, then we take in a shift value like the Ceaser cipher\n"
+          "but instead of changing the char, we just change its position so that all of the\n"
+          "chars have an offset of 'shift' places.")
+    Input = input("Please Provide a string that you would like to encrypt\n")
+    Input_List = Read_buffer.Convert_To_List(Input)
+    Shift = input("Please set the shift value to an integer of your choice. Note that the shift\n"
+                  "NEEDS to be smaller than the length of the string!\n")
+    Encoder.Physical_Shift_Encode(Input_List, Shift)
+    print("The Physical Shift encoded text is: " + Encoder.Output)
+    if Product_Queue.Decode == True:
+        Decoder.Physical_Shift_Decode(Encoder.Output, Shift)
+        print("Decoded Physical Shift encoded string: " + Decoder.Output)
+    else:
+        print("Decoding is currently disabled. Please enable if you would like to verify results.\n")
+    input("press any key to continue... \n")
+
+
+Encoder = Encoder()
+Decoder = Decoder()
+Key_buffer = Buffer()
+Read_buffer = Buffer()
+Product_Queue = Product()
+
+
+Welcome_Screen()
+Options()
+Options_Handling()
